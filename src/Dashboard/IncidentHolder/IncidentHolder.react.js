2026e8107a72a7ef8e201e1b47029e148e43483d
PK <anirudhpk97@gmail.com>
2019-09-10 17:49:56 +0530

    Update IncidentHolder.react.js
    Incidents show close and cancel only if active, else status will be shown

--- src/Dashboard/IncidentHolder/IncidentHolder.react.js
+++ src/Dashboard/IncidentHolder/IncidentHolder.react.js
@@ -4,23 +4,21 @@ import { Item, Button } from 'semantic-ui-react';
 
 //This module is used to hold individual incidents in the incident details page and the customer and agent dashboards
 const IncidentHolder = (props) => {
-
-    
-
     return (
             <Item>
                 <Item.Content>
                     <Item.Header>{props.details.comments}</Item.Header>
                     <Item.Description>
                             <p>This is where the description about the compliant/issue comes in.</p>
-                            <p>Issue Type: Active/Closed</p>
+                            <p>Issue Type: {props.details.status}</p>
                             <p>Issued by: Author Name</p>
                             <p>Issue added on: DD/MM/YYYY</p>
                     </Item.Description>
-                    <Item.Extra>
+                    {/* Logic to Display close and cancel if the incident is active */}
+                    {(props.details.status==='closed')? (<Item.Extra>
                         <Button color="blue" onClick={() => props.onClose(props.details.incidentID)}>Close Incident</Button>
                         <Button color="red" onClick={() => props.onCancel(props.details.incidentID)}>Cancel Incident</Button>
-                    </Item.Extra>
+                    </Item.Extra>):(<Item.Extra>This incident is closed</Item.Extra>)}    
                 </Item.Content>
             </Item>  
     );
